3b45edb3439e74f6f45d0cc32e78934c2d1a871f
naimur53 <naimurrhman53@gmail.com>
2023-07-16 17:13:42 +0600

    last commit

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import httpStatus from 'http-status';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routes from './app/routes';
 import cookieParser from 'cookie-parser';
-import moment from 'moment-timezone';
 const app: Application = express();
 
 app.use(cors());
@@ -21,13 +20,6 @@ app.use('/api/v1', routes);
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
 //   throw new Error('Testing Error logger')
 // })
-app.get('/current-time', (req, res) => {
-  const currentDateTime = new Date();
-  moment.tz.setDefault('Asia/Dhaka');
-  const mon = moment(currentDateTime);
-  res.json({ currentTime: currentDateTime, mon });
-});
-
 //global error handler
 app.use(globalErrorHandler);
 
